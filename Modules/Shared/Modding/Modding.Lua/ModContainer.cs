7f9c6820d6cccbce5093904c8b3f23a68e386fb2
Karpik <artem19051664@gmail.com>
2026-02-23 22:29:43 +0400

    Get filename once at LoadRootScripts in ModContainer

--- Modules/Shared/Modding/Modding.Lua/ModContainer.cs
+++ Modules/Shared/Modding/Modding.Lua/ModContainer.cs
@@ -155,6 +155,7 @@ public class ModContainer : IModContainer
 
             await foreach (var scriptFile in rootScripts.ToArray().ToAsyncEnumerable())
             {
+                string fileName = _assetsManager.FileSystem.GetFileName(scriptFile);
                 try
                 {
                     Script.DoStream(FileSystem.OpenRead(scriptFile));
@@ -163,33 +164,33 @@ public class ModContainer : IModContainer
                     if (updateFunction.IsNotNil() && updateFunction.Type == DataType.Function)
                     {
                         _updateFunction.Add(updateFunction);
-                        await Log($"Registered update for {_assetsManager.FileSystem.GetFileName(scriptFile)}");
+                        await Log($"Registered update for {fileName}");
                     }
                     
                     var startFunction = Script.Globals.Get(EventModMethods.OnStart);
                     if (startFunction.IsNotNil() && startFunction.Type == DataType.Function)
                     {
                         _startFunction.Add(startFunction);
-                        await Log($"Registered start for {_assetsManager.FileSystem.GetFileName(scriptFile)}");
+                        await Log($"Registered start for {fileName}");
                     }
                     
                     var loadFunction = Script.Globals.Get(EventModMethods.OnLoad);
                     if (loadFunction.IsNotNil() && loadFunction.Type == DataType.Function)
                     {
                         _loadFunction.Add(loadFunction);
-                        await Log($"Registered load for {_assetsManager.FileSystem.GetFileName(scriptFile)}");
+                        await Log($"Registered load for {fileName}");
                     }
                     
                     var unloadFunction = Script.Globals.Get(EventModMethods.OnUnload);
                     if (unloadFunction.IsNotNil() && unloadFunction.Type == DataType.Function)
                     {
                         _unloadFunction.Add(unloadFunction);
-                        await Log($"Registered unload for {_assetsManager.FileSystem.GetFileName(scriptFile)}");
+                        await Log($"Registered unload for {fileName}");
                     }
                 }
                 catch (Exception e)
                 {
-                    await Log($"Error loading {_assetsManager.FileSystem.GetFileName(scriptFile)}: {e}", LogLevel.Error);
+                    await Log($"Error loading {fileName}: {e}", LogLevel.Error);
                 }
             }
         }
